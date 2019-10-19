6ddcde6f690677ec7a3da422673e83bcdc94aef7
gtongy <32930281+gtongy@users.noreply.github.com>
2019-10-19 19:24:57 +0900

    :lipstick: modify basic infomation panel presentational

--- src/views/components/group/add/AddGroupForm.tsx
+++ src/views/components/group/add/AddGroupForm.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
+import BasicInfomationsPanel from './BasicInfomationsPanel'
 
 const AddGroupForm: React.FC<{}> = () => {
   return (
     <form>
       <Grid item xs={12}>
+        <BasicInfomationsPanel />
         <Button fullWidth variant="contained" color="primary" type="submit">
           グループ作成
         </Button>
